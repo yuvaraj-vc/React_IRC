5f2c3f23a461a4711c734ff26da3c5199a180b1a
yuvaraj-vc <vcyhari@gmail.com>
2024-01-02 16:28:45 +0530

    Day10

---
+++ musicplayer/musicapp/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Display from './Components/Display';
+
+function App() {
+  return (
+    <div className="App">
+   <Display/>
+    </div>
+  );
+}
+
+export default App;
